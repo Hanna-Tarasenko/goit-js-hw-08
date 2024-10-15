a8ed7a61aa4709a68378eb3e8e0c6f8c613c6d81
hanna_tarasenko <en.anya.tarasenko@gmail.com>
2024-10-15 22:58:10 +0300

    task8

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const listOfCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${listOfCategories.length}`);
-listOfCategories.forEach((element) => {
-  const elementName = element.querySelector("h2").textContent;
-  const elementNumber = element.querySelectorAll("li").length;
-  console.log(`Category: ${elementName}`);
-  console.log(`Elements: ${elementNumber}`);
-});
